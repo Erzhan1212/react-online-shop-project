adce6a2c41831eba6bddef3771cbe0f8881d4db3
ibadat <abdurasu10vaibd@gmail.com>
2024-11-25 19:10:05 +0300

    save footer

---
+++ src/assets/index.js
@@ -0,0 +1,37 @@
+import FooterLogoIcon from "./icon/brandName-icon.svg?react";
+import CartIcon from "./icon/cart-icon.svg?react";
+import CloseModalIcon from "./icon/close-icon.svg?react";
+import CopyProductIcon from "./icon/copy-icon.svg?react";
+import DeleteProductIcon from "./icon/delete-icon.svg?react";
+import EditProductIcon from "./icon/edit-icon.svg?react";
+import FooterIcon from "./icon/footer-icon.svg?react";
+import KidsIcon from "./icon/kids-icon.svg?react";
+import LoginIcon from "./icon/login-icon.svg?react";
+import MainLogoIcon from "./icon/mainLogo-icon.svg?react";
+import ManIcon from "./icon/man-icon.svg?react";
+import WomanIcon from "./icon/woman-icon.svg?react";
+import PaletteColorIcon from "./icon/palette-icon.svg?react";
+import PasswordEyeIcon from "./icon/privateEye-icon.svg?react";
+import PersonIcon from "./icon/person-icon.svg?react";
+import PlusProductIcon from "./icon/productPlus-icon.svg?react";
+import ProductPngIcon from "./icon/productPng-icon.svg?react";
+
+export const Icons = {
+  FooterLogoIcon,
+  CartIcon,
+  CloseModalIcon,
+  CopyProductIcon,
+  DeleteProductIcon,
+  EditProductIcon,
+  FooterIcon,
+  KidsIcon,
+  LoginIcon,
+  MainLogoIcon,
+  ManIcon,
+  WomanIcon,
+  PaletteColorIcon,
+  PasswordEyeIcon,
+  PersonIcon,
+  PlusProductIcon,
+  ProductPngIcon,
+};
